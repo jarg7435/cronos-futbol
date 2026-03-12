bf5c9393b268ff42d481bba5408a90b291438c1b
JARG74 <jarg74@ejemplo.com>
2026-03-12 02:46:01 +0000

    Bump cache version to v29 and fix auth.js inclusion

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'cronos-futbol-v1';
+const CACHE_NAME = 'cronos-futbol-v29';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './app.js',
+    './auth.js',
     './manifest.json',
     'https://cdn-icons-png.flaticon.com/512/53/53283.png'
 ];
@@ -12,6 +13,19 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
