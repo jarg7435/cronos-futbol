0ba0dfc594385a9674ea5c24d56c50820f575c47
jarg7435 <jarg7435@icloud.com>
2026-06-21 22:47:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -323,7 +323,7 @@
 // v142: SPRINT 4 — Offline Fallback + Local Icons
 // ─────────────────────────────────────────────────────────────
 const VERSION    = 'v191';
-const CACHE_NAME = 'cronos-cache-v192';
+const CACHE_NAME = 'cronos-cache-v193';
 
 const ASSETS = [
     './',
